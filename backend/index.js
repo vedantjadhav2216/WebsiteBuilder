573ac7f102b32bac628b211775dedae09627f28a
vedantjadhav2216 <vedantjadhav220106@gmail.com>
2026-07-14 22:05:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const PORT=process.env.PORT ||3000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://websitebuilder-1-nwxi.onrender.com",
     credentials:true
 }))
 
