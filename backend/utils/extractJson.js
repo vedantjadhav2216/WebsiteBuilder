13b57917ddb3288596f8f8639c21042ed4d90109
Vedant Jadhav <vedantjadhav220106@gmail.com>
2026-07-16 00:03:09 +0530

    Refactor extractJson to be an async function

--- backend/utils/extractJson.js
+++ backend/utils/extractJson.js
@@ -1,29 +1,17 @@
-const extractJson = (text) => {
-    if (!text) return null;
-
-    try {
-        const cleaned = text
-            .replace(/```json/gi, "")
-            .replace(/```/g, "")
-            .trim();
-
-        const start = cleaned.indexOf("{");
-        const end = cleaned.lastIndexOf("}");
-
-        if (start === -1 || end === -1) {
-            return null;
-        }
-
-        const json = cleaned.slice(start, end + 1);
-
-        return JSON.parse(json);
-
-    } catch (err) {
-        console.error("JSON Parse Error:");
-        console.error(err.message);
-
-        return null;
+const extractJson = async (text) => {
+    if (!text) {
+        return
     }
-};
+    const cleaned = text.
+        replace(/```json/gi, "")
+        .replace(/```/g, "")
+        .trim();
 
-export default extractJson;
+        const openBracket = cleaned.indexOf('{')
+        const closeBracket = cleaned.lastIndexOf('}')
+        if(openBracket === -1 || closeBracket === -1) return null
+        const jsonString = cleaned.slice(openBracket, closeBracket+1)
+        return JSON.parse(jsonString)
+}
+
+export default extractJson
