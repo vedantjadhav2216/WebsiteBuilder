66aef32ec2f090ab271a2b8af81d9dc04f61fb99
Vedant Jadhav <vedantjadhav220106@gmail.com>
2026-07-16 00:00:51 +0530

    Refactor generateResponse function for clarity

--- backend/config/openRouter.js
+++ backend/config/openRouter.js
@@ -1,37 +1,33 @@
+const openRouterUrl = 'https://openrouter.ai/api/v1/chat/completions'
+const model = 'deepseek/deepseek-chat'
 
-
-export const generateResponse=async(prompt)=>
-{
-    const res=await fetch('https://openrouter.ai/api/v1/chat/completions', {
-  method: 'POST',
-  headers: {
-    Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-  model: "deepseek/deepseek-chat-v3-0324",
-  messages: [
-    {
-      role: "system",
-      content: "You must return only valid raw json"
-    },
-    {
-      role: "user",
-      content: prompt
-    }
-  ],
-  temperature: 0.2,
-  max_tokens: 2500
-}),
-});
-
-    if(!res.ok)
-    {
-        const err=await res.text()
-        throw new Error("openrouter err"+err)
+export const generateResponse = async (prompt) => {
+    const res = await fetch('https://openrouter.ai/api/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            model: 'deepseek/deepseek-chat',
+            messages: [
+                {
+                    role: 'system',
+                    content: "You must return only valid raw JSON",
+                },
+                {
+                    role: 'user',
+                    content: prompt,
+                },
+            ],
+            temperature:0.2
+        }),
+    });
+    if(!res.ok){
+        const err = await res.text()
+        throw new Error("OpenRouter err"+ err)
     }
 
-    const data=await res.json()
+    const data = await res.json()
     return data.choices[0].message.content
-
 }
